d3680b8bbfde38e7dbabbc60640ccbee06dd9bd1
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-04-22 23:48:36 +0900

    Support hr styled blog

--- app.rb
+++ app.rb
@@ -1,17 +1,29 @@
 require 'sinatra'
 require 'rss'
 require 'open-uri'
+require 'cgi'
 require_relative './destructor'
-
-ORIGIN_URL = 'https://ruby-trunk-changes.hatenablog.com/rss'
 
 configure do
   mime_type :rss, 'application/rss+xml; charset=UTF-8'
 end
 
 get '/rss' do
+  mode = params[:mode]&.to_sym
+  unless mode
+    status 400
+    return "mode param is required"
+  end
+
+  origin_url = params[:origin_url]&.then { |url| CGI.unescape(url) }
+  unless origin_url
+    status 400
+    return 'origin_url param is required'
+  end
+
   content_type :rss
-  content = open(ORIGIN_URL) { |io| io.read }
+
+  content = open(origin_url) { |io| io.read }
   orig_rss = RSS::Parser.parse(content)
-  Destructor.destruct(orig_rss).to_s
+  Destructor.destruct(orig_rss, mode: mode).to_s
 end
--- destructor.rb
+++ destructor.rb
@@ -1,12 +1,13 @@
 require 'nokogiri'
 
 class Destructor
-  def self.destruct(rss)
-    self.new(rss).destruct
+  def self.destruct(rss, mode:)
+    self.new(rss, mode: mode).destruct
   end
 
-  def initialize(rss)
+  def initialize(rss, mode:)
     @rss = rss
+    @mode = mode
   end
 
   def destruct
@@ -18,22 +19,74 @@ class Destructor
       maker.items.do_sort = true
 
       rss.channel.items.each do |item|
-        doc = Nokogiri::HTML(item.description)
-        doc.css('div.section').each.with_index do |section, index|
-          title = section.css('h1, h2, h3, h4, h5, h6').text
-          description = section.css('p').text
-
-          maker.items.new_item do |new_item|
-            new_item.link = item.link
-            new_item.date = item.date + index
-            new_item.title = title
-            new_item.description = description
-          end
+        case mode
+        when :header
+          destruct_header(maker, item)
+        when :hr
+          destruct_hr(maker, item)
         end
       end
     end
   end
 
-  attr_reader :rss
-  private :rss
+  def destruct_header(maker, item)
+    doc = Nokogiri::HTML(item.description)
+    doc.css('div.section').each.with_index do |section, index|
+      title = section.css('h1, h2, h3, h4, h5, h6').text
+      description = section.css('p').text
+      make_new_item(
+        maker: maker,
+        link: item.link,
+        date: item.date + index,
+        title: title,
+        description: description,
+      )
+    end
+  end
+
+  def destruct_hr(maker, item)
+    doc = Nokogiri::HTML(item.description)
+    buf = +""
+    index = 0
+    doc.traverse do |node|
+      case
+      when node.text?
+        buf << node.text
+      when node.name == "hr"
+        p buf
+        make_new_item(
+          maker: maker,
+          link: item.link,
+          date: item.date + index,
+          title: '--',
+          description: buf,
+        )
+
+        buf = +""
+        index += 1
+      end
+    end
+
+    if buf =~ /\S/
+      make_new_item(
+        maker: maker,
+        link: item.link,
+        date: item.date + index,
+        title: '--',
+        description: buf,
+      )
+    end
+  end
+
+  def make_new_item(maker:, link:, date:, title:, description:)
+    maker.items.new_item do |new_item|
+      new_item.link = link
+      new_item.date = date
+      new_item.title = title
+      new_item.description = description
+    end
+  end
+
+  attr_reader :rss, :mode
+  private :rss, :mode
 end
